72ae411239d084feb5254d4768e80d61c8d4bb2d
cuidianjay <cuidianjay@163.com>
2019-09-26 15:06:26 +0800

    ss

--- cicd/run.sh
+++ cicd/run.sh
@@ -4,47 +4,8 @@ args='-Dspring.profiles.active=test'
 cmd=$1
 pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
 log='/usr/local/jay/go-agent-19.8.0/logs/nohup.out'
+applog='/usr/local/jay/go-agent-19.8.0/logs/spring.log'
 
-startup(){
-  nohup java -jar $args $app > $log &
-  echo "$app run success"
-  exit
-  #tail -f nohup.out
-}
-
-if [ ! $cmd ]; then
-  echo "Please specify args 'start|restart|stop'"
-  exit
-fi
-
-if [ $cmd == 'start' ]; then
-  if [ ! $pid ]; then
-    startup
-  else
-    echo "$app is running! pid=$pid"
-  fi
-  exit
-fi
-
-if [ $cmd == 'restart' ]; then
-  if [ $pid ]
-    then
-      echo "$pid will be killed after 2 seconds!"
-      sleep 2
-      kill -9 $pid
-  fi
-  startup
-  exit
-fi
-
-if [ $cmd == 'stop' ]; then
-  if [ $pid ]; then
-    echo "$pid will be killed after 2 seconds!"
-    sleep 2
-    kill -9 $pid
-  fi
-  echo "$app is stopped"
-  exit
-fi
-
-exit 0
+nohup java -jar $args $app > $log &
+echo "$app run success"
+tail -f $applog
