17cd60a0237e76243c960bbdbccd2d1bf90705bc
cuidianjay <cuidianjay@163.com>
2019-09-26 09:31:23 +0800

    #ME-267 修改run.sh重启脚本

--- cicd/run.sh
+++ cicd/run.sh
@@ -1,1 +1,43 @@
-java -jar cicd-0.0.1-SNAPSHOT.jar &
+#!/bin/bash
+app='cicd.jar'
+args=
+cmd=$1
+pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
+
+startup(){
+  #nohup java -jar $args $app &	
+  java -jar $args $app &
+  #tail -f nohup.out
+}
+
+if [ ! $cmd ]; then
+  echo "Please specify args 'start|restart|stop'"
+  exit
+fi
+
+if [ $cmd == 'start' ]; then
+  if [ ! $pid ]; then
+    startup
+  else
+    echo "$app is running! pid=$pid"
+  fi
+fi
+
+if [ $cmd == 'restart' ]; then
+  if [ $pid ]
+    then
+      echo "$pid will be killed after 2 seconds!"
+      sleep 2
+      kill -9 $pid
+  fi
+  startup
+fi
+
+if [ $cmd == 'stop' ]; then
+  if [ $pid ]; then
+    echo "$pid will be killed after 2 seconds!"
+    sleep 2
+    kill -9 $pid
+  fi
+  echo "$app is stopped"
+fi
