f148832210361efccb2927c01aa91c791b0509ab
cuidianjay <cuidianjay@163.com>
2019-09-26 15:19:57 +0800

    test

--- cicd/run.sh
+++ cicd/run.sh
@@ -6,6 +6,9 @@ pid=`ps -ef|grep java|grep $app|awk '{print $2}'`
 log='/usr/local/jay/go-agent-19.8.0/logs/nohup.out'
 applog='/usr/local/jay/go-agent-19.8.0/logs/spring.log'
 
+if [ $pid ]; then
+	kill -9 $pid
+fi
 nohup java -jar $args $app > $log &
 echo "$app run success"
-tail -f $applog
+return 0
